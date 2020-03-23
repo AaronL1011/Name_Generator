2090a686157b3f3eca0f650c07842e1c4fa2c5ab
Aaron Lewis <aaron.lewis1011@gmail.com>
2020-03-24 09:33:12 +1100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,14 @@
+## Requirements
+
+Ensure you have these two gems installed:
+```
+gem install tty-progressbar
+gem install artii
+```
+
+## Usage
+
+Execute the namegenerator.rb file from your terminal!
+```
+ruby namegenerator.rb
+```
---
+++ namegenerator.rb
@@ -0,0 +1,53 @@
+require 'artii'
+require 'tty-progressbar'
+
+def chooseName()
+    all_the_humans = [
+        "Aaron Lewis",
+        "Adam Payne",
+        "Andrew Tran",
+        "Anh Nguyen",
+        "Ashley Bridges",
+        "Christopher Morris",
+        "Vicky Pham",
+        "Esther Csoke",
+        "Evelyn Paplauskas",
+        "Gary Ho",
+        "Gregory Martin",
+        "Harrison Powell",
+        "Harry Buisman",
+        "Hoang Hai Ho",
+        "Hugh Greethead",
+        "Jacob Colyvan",
+        "Jeremy Wu",
+        "Linda Ojinnaka",
+        "Ling Yen Lee",
+        "Martin Kennedy",
+        "Martin Valdivia",
+        "Matthew Molloy",
+        "Morgan Webb",
+        "Morgan Batterham",
+        "Nikki Waelkens",
+        "Oliver Mason",
+        "Patricia Pavia",
+        "Rederyck Vazquez",
+        "Rose Hernandez",
+        "Ross Baker",
+        "Ryan Cook",
+        "Vincent Lieu",
+        "Gini Thomas",
+        "Wendy Doan",
+        "Wilkins Cheng"]        
+    puts `clear`
+    randomChoice = all_the_humans[rand(all_the_humans.length - 1)]
+    bar = TTY::ProgressBar.new("Preparing a name... [:bar]", total: 30)
+    30.times do
+      sleep(2)
+      bar.advance(1)
+    end
+    a = Artii::Base.new :font => 'slant'
+    puts a.asciify(randomChoice)
+end
+
+
+chooseName()
